92ce8a4c63877f8e5c16f32d2b4b4ec7ec1beef4
Eloiza <eloiza.souza@zup.com.br>
2024-12-23 16:19:15 -0300

    test: add TestEnumHandler

--- src/test/TestEnumHandler.java
+++ src/test/TestEnumHandler.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+package test;
+
+import model.Category;
+import util.EnumHandler;
+
+import static util.EnumHandler.printAllValues;
 
 public class TestEnumHandler {
+    public static void main(String[] args) {
+
+        printAllValues(Category.class);
+    }
 }
+
