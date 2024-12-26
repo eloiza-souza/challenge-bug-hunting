8222cc205ad8049807d5a6c1f1cfcf4f4c208d65
Eloiza <eloiza.souza@zup.com.br>
2024-12-26 14:42:16 -0300

    feat: add new items to PrincipalMenu

--- src/model/PrincipalMenu.java
+++ src/model/PrincipalMenu.java
@@ -5,6 +5,11 @@ public enum PrincipalMenu {
     ADD("Adicionar vídeo"),
     LIST("Listar vídeos"),
     SEARCH_BY_TITLE("Pesquisar vídeo por título"),
+    EDIT("Editar vídeo"),
+    DELETE("Excluir vídeo"),
+    VIDEOS_BY_CATEGORY("Filtrar vídeos por categoria"),
+    SORT_BY_DATE("Ordenar vídeos por data de publicação"),
+    REPORT ("Exibir relatório de estatísticas"),
     EXIT("Sair");
 
     private String description;
@@ -17,6 +22,5 @@ public enum PrincipalMenu {
     public String toString() {
         return description;
     }
-
 
 }
