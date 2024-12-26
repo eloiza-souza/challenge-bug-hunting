d7e47783a97e79183fbdb08df370b8af88b79085
Eloiza <eloiza.souza@zup.com.br>
2024-12-26 09:51:38 -0300

    refactor: add VideoController

--- src/controller/VideoController.java
+++ src/controller/VideoController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import model.Menu;
+
+import java.util.Scanner;
+
 public class VideoController {
+    final int EXIT = Menu.values().length + 1;
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+
+        showMenu();
+
+        scanner.close();
+    }
+
+    private void showMenu() {
+        System.out.println("\nEscolha uma opção:");
+        int index = 1;
+        for (Menu option : Menu.values()) {
+            System.out.println(index++ + ". " + option.getDescription());
+        }
+        System.out.println(EXIT + ". Sair");
+    }
+
 }
---
+++ src/test/TestVideoController.java
@@ -0,0 +1,13 @@
+package test;
+
+import controller.VideoController;
+
+public class TestVideoController {
+    public static void main(String[] args) {
+        VideoController videoController = new VideoController();
+        videoController.run();
+    }
+
+
+
+}
