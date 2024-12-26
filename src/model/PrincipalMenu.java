3eb0d382504741aba0593631eb70f505a45ed010
Eloiza <eloiza.souza@zup.com.br>
2024-12-26 12:35:32 -0300

    style: rename class Menu to PrincipalMenu

--- src/model/PrincipalMenu.java
+++ src/model/PrincipalMenu.java
@@ -1,21 +1,18 @@
 package model;
 
-public enum Menu {
+public enum PrincipalMenu {
 
-    ADD ("Adicionar vídeo"),
-    LIST ("Listar vídeos"),
-    SEARCH_BY_TITLE ("Pesquisar vídeo por título"),
-    EXIT ("Sair");
+    ADD("Adicionar vídeo"),
+    LIST("Listar vídeos"),
+    SEARCH_BY_TITLE("Pesquisar vídeo por título"),
+    EXIT("Sair");
 
     private String description;
 
-    Menu(String description) {
+    PrincipalMenu(String description) {
         this.description = description;
     }
 
-    public String getDescription() {
-        return description;
-    }
     @Override
     public String toString() {
         return description;
