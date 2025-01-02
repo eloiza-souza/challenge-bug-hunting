4a99d42e1dc33c4bfd6bb0921ca35a2e1bdc5b8b
Eloiza <eloiza.souza@zup.com.br>
2025-01-02 10:25:22 -0300

    refactor: transform DateUtil  in abstract class

--- src/util/DateUtil.java
+++ src/util/DateUtil.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateUtil {
+public abstract class DateUtil {
     public static final String DATE_FORMAT = "dd/MM/yyyy";
 
     public static boolean isValidDate(String dateInput) {
