c8258f31240b924ffff4ed04efbb148b7e3666a0
Eloiza <eloiza.souza@zup.com.br>
2025-01-02 10:23:18 -0300

    refactor: transform VideoManager in abstract class

--- src/service/VideoManager.java
+++ src/service/VideoManager.java
@@ -12,7 +12,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-public class VideoManager {
+public abstract class VideoManager {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
 
     public static Video createVideo(Scanner scanner) {
